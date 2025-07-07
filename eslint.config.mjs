b5c707f52a4ee9e3385e3b8d715476294f73a062
Ronald <ronaldzamoradev@gmail.com>
2025-07-07 18:39:21 -0500

    clean

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,32 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "comma-dangle": ["error", "never"],
+      semi: ["error", "never"],
+      "react-hooks/exhaustive-deps": "off",
+      "func-call-spacing": ["error", "never"],
+      eqeqeq: ["error", "always"],
+      "no-var": "error",
+      "prefer-const": "error",
+      "object-curly-spacing": ["error", "always"],
+      "array-bracket-spacing": ["error", "never"],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-loop-func": "error",
+      "no-eval": "error",
+      "prefer-template": "error",
+      "no-dupe-keys": "error",
+      "no-duplicate-case": "error",
+      "no-multi-spaces": "error",
+      "no-trailing-spaces": "error",
+      "space-infix-ops": "error",
+      "keyword-spacing": "error",
+      "comma-spacing": ["error", { before: false, after: true }],
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-explicit-any": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
